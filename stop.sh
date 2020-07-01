ff845a293765ec6f43aa2f55ddcd97301ecd3aca
Charles Holtzkampf <charles.holtzkampf@gmail.com>
2020-07-01 21:04:28 +0100

    Update stop.sh

--- stop.sh
+++ stop.sh
@@ -3,8 +3,8 @@
 #
 # Scrip Created by http://CryptoLions.io
 # For EOS mainnet
-#
-# https://github.com/CryptoLions/EOS-MainNet
+# & Updated by Charles Holtzkampf
+# https://github.com/ankh2054/EOS-MainNet
 #
 ###############################################################################
 
@@ -12,25 +12,25 @@ DIR="/chains/wax"
 
 
  if [ -f $DIR"/nodeos.pid" ]; then
-	pid=`cat $DIR"/nodeos.pid"`
-	echo $pid
-	kill $pid
-	
+        pid=`cat $DIR"/nodeos.pid"`
+        echo $pid
+        kill $pid
 
-	echo -ne "Stoping Nodeos"
+
+        echo -ne "Stoping Nodeos"
 
         while true; do
             [ ! -d "/proc/$pid/fd" ] && break
             echo -ne "."
             sleep 1
         done
-	rm -r $DIR"/nodeos.pid"
-	
-	DATE=$(date -d "now" +'%Y_%m_%d-%H_%M')
+        rm -r $DIR"/nodeos.pid"
+
+        DATE=$(date -d "now" +'%Y_%m_%d-%H_%M')
         if [ ! -d $DIR/logs ]; then
             mkdir $DIR/logs
         fi
-        tar -pcvzf $DIR/logs/stderr-$DATE.txt.tar.gz stderr.txt stdout.txt
+        tar -pcvzf $DIR/logs/stderr-$DATE.txt.tar.gz  $DIR/stderr.txt $DIR/stdout.txt &>/dev/null
 
 
         echo -ne "\rNodeos Stopped.    \n"
